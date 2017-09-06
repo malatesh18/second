01241083a83b2d21d87e36f2230d67290c8bd71b
ab94621 <ab94621@SGH638RJZ3.ctl.intranet>
2017-09-06 17:27:11 +0530

    again args

--- git/src/git/first.java
+++ git/src/git/first.java
@@ -5,7 +5,7 @@ public class first {
 	public static void main(String[] args) {
 System.out.println("main");
 System.out.println("args");
-
+System.out.println("args");System.out.println("args");
 	}
 
 }
